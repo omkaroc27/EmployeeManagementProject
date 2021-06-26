3bdcab486faf13d012e8a97abe17a1acc8c0871d
omkaroc27 <77641172+omkaroc27@users.noreply.github.com>
2021-06-26 20:14:35 +0530

    Add files via upload

---
+++ Source/EmployeeEMS.sql
@@ -0,0 +1,18 @@
+create database employee;
+use employee;
+
+create table feedback(
+name varchar(40)  not null ,
+email varchar (40) not null,
+phoneno varchar (40) not null,
+companyname varchar(40) not null,
+feedbackmsg varchar(30)
+)
+create table employeedata(
+empname varchar(40)  not null ,
+empemail varchar (40) not null,
+address varchar(40)  not null ,
+departmentid varchar (40) not null,
+phone varchar (40) not null
+)
+
